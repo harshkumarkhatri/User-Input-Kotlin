59a73e2541be1a94727c4b1e4336b25ab3b523f7
harshkumarkhatri <mailharshkhatri@gmail.com>
2020-05-16 20:12:43 +0530

    Completed the app with keyboard auto SoftInput.

--- app/src/main/java/com/android/example/AboutMe/MainActivity.kt
+++ app/src/main/java/com/android/example/AboutMe/MainActivity.kt
@@ -19,6 +19,10 @@ class MainActivity : AppCompatActivity() {
         findViewById<Button>(R.id.done_button).setOnClickListener{
             addNickname(it)
         }
+        findViewById<TextView>(R.id.nickname_text).setOnClickListener{
+            updateNickname(it)
+
+        }
     }
     private fun addNickname(view:View){
         val editText=findViewById<EditText>(R.id.nickname_edit)
@@ -29,5 +33,16 @@ class MainActivity : AppCompatActivity() {
         nicknameTextView.visibility= View.VISIBLE
         val inputMethodManager=getSystemService(Context.INPUT_METHOD_SERVICE) as InputMethodManager
         inputMethodManager.hideSoftInputFromWindow(view.windowToken,0)
+    }
+    private fun updateNickname(view :View){
+        val editText=findViewById<EditText>(R.id.nickname_edit)
+        val doneButton=findViewById<Button>(R.id.done_button)
+        editText.visibility=View.VISIBLE
+        doneButton.visibility=View.VISIBLE
+        view.visibility=View.GONE
+        editText.requestFocus()
+        val inn=getSystemService(Context.INPUT_METHOD_SERVICE) as InputMethodManager
+        inn.showSoftInput(editText,0)
+
     }
 }
